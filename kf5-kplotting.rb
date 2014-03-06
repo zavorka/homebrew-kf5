3ee7ed8c2d4f8796ce03c5a5f90dc8d3dfa7cacd
Harald Fernengel <harry@kdevelop.org>
2014-03-06 20:21:33 +0100

    add kplotting

---
+++ kf5-kplotting.rb
@@ -0,0 +1,22 @@
+require "formula"
+
+class Kf5Kplotting < Formula
+  url "http://download.kde.org/unstable/frameworks/4.97.0/kplotting-4.97.0.tar.xz"
+  sha1 "3f715abce02ce1d651d0e4a7d80a602232146eb9"
+  homepage "http://www.kde.org/"
+
+  head 'git://anongit.kde.org/kplotting.git'
+
+  depends_on "cmake" => :build
+  depends_on "haraldf/kf5/kf5-extra-cmake-modules" => :build
+  depends_on "qt5" => "with-d-bus"
+
+  def install
+    args = std_cmake_args
+
+    args << "-DCMAKE_CXX_FLAGS='-D_DARWIN_C_SOURCE'"
+
+    system "cmake", ".", *args
+    system "make", "install"
+  end
+end
